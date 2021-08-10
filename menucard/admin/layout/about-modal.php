b25e6949ebd402a7d50305cb558c5c1adffd51a1
TIYASHA DASGUPTA <68747850+TIYASHADASGUPTA@users.noreply.github.com>
2021-08-10 23:18:51 +0530

    Add files via upload

---
+++ menucard/admin/layout/about-modal.php
@@ -0,0 +1,9 @@
+<div id="modal1" class="modal">
+    <div class="modal-content">
+      <h4>About Cafe Zone</h4>
+      <p>We love to make delicious foods and serve our customers.</p>
+    </div>
+    <div class="modal-footer">
+      <a href="#!" class="modal-close waves-effect waves-green btn-flat">Close</a>
+    </div>
+  </div>-->
---
+++ menucard/chunks/reviews.php
@@ -0,0 +1,78 @@
+<section class="freviews">
+
+		<div class="section white center">
+			<h3 class="header">What Our Customers Say</h3>
+			<div class="carousel myreviews" style="margin-bottom: 35px;">
+			    <a class="carousel-item" href="#one!">
+			    	<div class="row">
+					    <div class="col s12">
+					      <div class="card-panel teal" style="background:  #85adad !important;">
+					        <span class="white-text">"The food of this resturant is just like heaven for me! Its so delicious and tasty that I can't help going there every weekend!"<br>-<br> <strong>Cust 1</strong>
+					        </span>
+					      </div>
+					    </div>
+					  </div>
+			    </a>
+			    <a class="carousel-item" href="#two!">
+			    	<div class="row">
+					    <div class="col s12">
+					      <div class="card-panel teal" style="background:  #999999 !important;">
+					        <span class="white-text">"The food of this resturant is just like heaven for me! Its so delicious and tasty that I can't help going there every weekend!"<br>-<br> <strong>Cust 2</strong>
+					        </span>
+					      </div>
+					    </div>
+					  </div>
+			    </a>
+			    <a class="carousel-item" href="#three!">
+			    	<div class="row">
+					    <div class="col s12">
+					      <div class="card-panel teal" style="background: #ff4dff !important;">
+					        <span class="white-text">"The food of this resturant is just like heaven for me! Its so delicious and tasty that I can't help going there every weekend!"<br>-<br> <strong>Cust 3</strong>
+					        </span>
+					      </div>
+					    </div>
+					  </div>
+			    </a>
+			    <a class="carousel-item" href="#four!">
+			    	<div class="row">
+					    <div class="col s12">
+					      <div class="card-panel teal" style="background: #ffa366 !important;">
+					        <span class="white-text">"The food of this resturant is just like heaven for me! Its so delicious and tasty that I can't help going there every weekend!"<br>-<br> <strong>Cust 4</strong>
+					        </span>
+					      </div>
+					    </div>
+					  </div>
+			    </a>
+			    <a class="carousel-item" href="#five!">
+			    	<div class="row">
+					    <div class="col s12">
+					      <div class="card-panel teal" style="background:#00b300 !important;">
+					        <span class="white-text">"The food of this resturant is just like heaven for me! Its so delicious and tasty that I can't help going there every weekend!"<br>-<br> <strong>Cust 5</strong>
+					        </span>
+					      </div>
+					    </div>
+					  </div>
+			    </a>
+			    <a class="carousel-item" href="#six!">
+			    	<div class="row">
+					    <div class="col s12">
+					      <div class="card-panel teal" style="background:#ff9966 !important;">
+					        <span class="white-text">"The food of this resturant is just like heaven for me! Its so delicious and tasty that I can't help going there every weekend!"<br>-<br> <strong>Cust 6</strong>
+					        </span>
+					      </div>
+					    </div>
+					  </div>
+			    </a>
+			    <a class="carousel-item" href="#seven!">
+			    	<div class="row">
+					    <div class="col s12">
+					      <div class="card-panel teal" style="background:#6666ff !important;">
+					        <span class="white-text">"The food of this resturant is just like heaven for me! Its so delicious and tasty that I can't help going there every weekend!"<br>-<br> <strong>Cust 7</strong>
+					        </span>
+					      </div>
+					    </div>
+					  </div>
+			    </a>
+			  </div>
+		</div>
+	</section>
